0558a4fd4944a57b01bb4c1d58ab395404b26cdc
madhvi baghel <71016771+madhvi-baghel@users.noreply.github.com>
2023-12-21 12:22:31 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3 + 3) //we are use nodejs, not browser
+
+console.log(3 // these type of prectise is not allowed it is not look proper or appropriate
+    + 
+    3) //code readability should be high
+
+ console.log("madhvi")
+
+
+ let name = "madhvi" //string
+ let age = 18 //number
+ let isLoggedIn= false //boolean
+ let state = null //null means empty
+
+ // number => 2 to power 53
+ //bigint
+ //string = " " ,' '
+ //bolean => true/false
+ //null => standalone value
+ //undefined =>mtlb abhi value assign nhi hui he
+ //symbol => unique 
+
+
+ //object
+
+ console.log(typeof null); //output => object
+ console.log(typeof undefined); //output => undefined
+
+
