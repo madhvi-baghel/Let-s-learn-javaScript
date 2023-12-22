927226ff76f65acfb48fc8fba7524cbdf29f08c4
madhvi baghel <71016771+madhvi-baghel@users.noreply.github.com>
2023-12-22 02:46:13 +0000

    compare

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);//greater than //true
+// console.log(2 >= 1);//true
+// console.log(2 < 1);//false
+// console.log(2 == 1);//false
+// console.log(2 != 1);//true
+
+
+// console.log("2" > 1);//true
+// console.log("02" > 1);//true
+
+// console.log(null > 0);//false
+// console.log(null == 0);//false
+// console.log(null >= 0);//true //0 >= 0 in this operator its convert null as 0 but it might be convert in NaN also //avoid these type of comparision
+
+// console.log(undefined == 0);//false
+// console.log(undefined > 0);//false
+// console.log(undefined < 0);//false
+
+// ===
+
+console.log("2" === 0);//false ye strictly check karta he (datType same hona chahiye)
